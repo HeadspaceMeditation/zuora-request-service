d8bd0e58ab1bef30c1696206eed4d53dde041b51
Phu Huynh <phu@headspace.com>
2018-06-04 21:10:41 -0700

    Add create refund method

---
+++ zuora-api/test/refunds.test.js
@@ -0,0 +1,31 @@
+/* eslint no-unused-expressions: 0, init-declarations: 0, prefer-arrow-callback: 0, func-names: 0, max-statements: 0 */
+
+'use strict';
+
+const proxyquire = require('proxyquire');
+
+describe('refunds', function() {
+  let idStub;
+  let requestStub;
+  let refunds;
+  let requestOptionsStub;
+
+  beforeEach(function() {
+    requestStub = sinon.stub();
+    idStub = 1234;  // eslint-disable-line no-magic-numbers
+    requestOptionsStub = {
+        PaymentId: 'paymentId',
+        Type: 'Electronic',
+        Amount: 12,
+    };
+
+    refunds = proxyquire('../refunds', {
+      './proxied-request': requestStub
+    });
+  });
+
+  it('create calls proxy request with the correct paramaters', function() {
+    refunds.create(requestOptionsStub);
+    expect(requestStub).to.have.been.calledWithExactly('POST', 'object/refund', requestOptionsStub);
+  });
+});
